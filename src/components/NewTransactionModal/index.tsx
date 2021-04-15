48845bc8074f10838cabafea8806339b0971a089
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-04-15 16:46:10 -0300

    feat: send transaction data to API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,7 @@ import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import { FormEvent, useState } from 'react'
+import { api } from '../../services/api'
 
 interface NewTransactionModalProps {
   isOpen: boolean
@@ -24,12 +25,14 @@ export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModal
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
 
-    console.log(
+    const data = {
       type,
       title,
       value,
       category
-    )
+    }
+
+    api.post('/transactions', data)
   }
 
   return (
