8de900b23a46d5ddcff9cdd148d9828063420513
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-04-16 20:11:55 -0300

    refactor: remove comments

--- src/index.tsx
+++ src/index.tsx
@@ -47,58 +47,6 @@ createServer({
   },
 });
 
-// createServer({
-
-//   models: {
-//     transaction: Model
-//   },
-
-//   seeds(server) {
-//     server.db.loadData ({
-//       transactions: [
-//         {
-//           id: 1,
-//           title: 'freela',
-//           type: 'deposit',
-//           category: 'Dev',
-//           amount: 6000,
-//           createdAt: new Date('2021-02-19 09:00:00')
-//         },
-//         {
-//           id: 2,
-//           title: 'Internet',
-//           type: 'withdraw',
-//           category: 'Despesas',
-//           amount: 100,
-//           createdAt: new Date('2021-02-20 09:00:00')
-//         },
-//         {
-//           id: 3,
-//           title: 'Lanche',
-//           type: 'withdraw',
-//           category: 'Alimentação',
-//           amount: 50,
-//           createdAt: new Date('2021-02-20 12:00:00')
-//         },
-//       ]
-//     })
-//   },
-
-//   routes() {
-//     this.namespace = 'api'
-
-//     this.get('/transactions', () => {
-//       return this.schema.all('transaction')
-//     })
-
-//     this.post('/transactions', (schema, request) => {
-//       const data = JSON.parse(request.requestBody)
-//       //  console.log({mirage: data})
-//       //  return this.schema.create('transaction', data)
-//       return this.schema.create('transaction', data)
-//     })
-//   }
-// })
 
 ReactDOM.render(
   <React.StrictMode>
